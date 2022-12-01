e2e1d6c8b4493258155504d9e2abd17586b9acc5
Hitalo <hitalo.2014.hs@gmail.com>
2022-12-01 18:19:32 -0300

    feat: backend finished

--- back-end/src/routes/contacts.routes.ts
+++ back-end/src/routes/contacts.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import contactCreateController from "../controllers/contacts/contactCreate.controller";
+import contactDeleteController from "../controllers/contacts/contactDelete.controller";
 import contactReadController from "../controllers/contacts/contactRead.controller";
 import contactUpdateController from "../controllers/contacts/contactUpdate.controller";
 import authMiddleware from "../middlewares/auth.middleware";
@@ -9,5 +10,6 @@ export function contactsRouters() {
   routes.post("", authMiddleware, contactCreateController);
   routes.get("", authMiddleware, contactReadController);
   routes.patch("/:id", authMiddleware, contactUpdateController);
+  routes.delete("/:id", authMiddleware, contactDeleteController);
   return routes;
 }
--- back-end/src/services/contacts/contactDelete.services.ts
+++ back-end/src/services/contacts/contactDelete.services.ts
@@ -1,19 +1,15 @@
-import { v4 } from "uuid";
 import { AppDataSource } from "../../data-source";
 import { Client } from "../../entities/clients.entity";
 import { Contact } from "../../entities/contact.entity";
 import { AppError } from "../../errors/appError";
-import { IContactUpdate } from "../../interfaces/contact";
 import { validate as uuidValidate } from "uuid";
 
-async function contactUpdateService(
-  clientId: string,
+async function contactDeleteService(
   contactId: string,
-  { email, fullName, phone }: IContactUpdate
-): Promise<Contact> {
+  clientId: string
+): Promise<string> {
   const contactRepository = AppDataSource.getRepository(Contact);
   const clientRepository = AppDataSource.getRepository(Client);
-
   if (!uuidValidate(contactId)) throw new AppError(400, "Invalid uuid param.");
 
   const client = await clientRepository.findOneBy({ id: clientId });
@@ -23,21 +19,14 @@ async function contactUpdateService(
     where: {
       id: contactId,
       client: {
-        id: client.id,
+        id: clientId,
       },
     },
   });
   if (!contact) throw new AppError(404, "Contact not found.");
 
-  await contactRepository.update(contact.id, {
-    email: email ? email : contact.email,
-    fullName: fullName ? fullName : contact.fullName,
-    phone: phone ? phone : contact.phone,
-  });
-
-  const contactReturn = await contactRepository.findOneBy({ id: contact.id });
-
-  return contactReturn!;
+  await contactRepository.delete(contact.id);
+  return "Contact deleted with success.";
 }
 
-export default contactUpdateService;
+export default contactDeleteService;
